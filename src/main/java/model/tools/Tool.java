a3c16f6d4c4c00102d1a4230e5a8b9d0ae0d752b
Jonas Leijzen <jonasleijzen@gmail.com>
2021-10-27 20:57:17 +0200

    Added basic functionality to Tool

--- src/main/java/model/tools/Tool.java
+++ src/main/java/model/tools/Tool.java
@@ -1,8 +1,61 @@
 package model.tools;
 
+import model.Lendable;
+
 /**
  * Anouar Bannamar
  * 27-10-21
  */
-public class Tool {
+public class Tool extends Lendable {
+	
+	private String name;
+	private String description;
+	private ToolType type;
+	private double price;
+	
+	public Tool (String name, String description, ToolType type, double price) {
+		setName (name);
+		setDescription (description);
+		setToolType (type);
+		setPrice (price);
+	}
+	
+	public void setName (String name) {
+		if (name == null || name.length () == 0)
+			throw new IllegalArgumentException ("Name cannot be null or empty");
+		this.name = name;
+	}
+	
+	public void setDescription (String description) {
+		if (description == null || description.length () == 0)
+			throw new IllegalArgumentException ("Description cannot be null or empty");
+		this.description = description;
+	}
+	
+	public void setToolType (ToolType type) {
+		this.type = type;
+	}
+	
+	public void setPrice (double price) {
+		if (price < 0)
+			throw new IllegalArgumentException ("Price cannot be negative.");
+		this.price = price;
+	}
+	
+	public String getName () {
+		return name;
+	}
+	
+	public String getDescription () {
+		return description;
+	}
+	
+	public ToolType getType () {
+		return type;
+	}
+	
+	@Override
+	public double getPrice () {
+		return price;
+	}
 }
