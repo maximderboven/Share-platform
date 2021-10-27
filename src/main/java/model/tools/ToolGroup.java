78f2055099feeea7168e1ce36cc4dbeab7fd10c5
Jonas Leijzen <jonasleijzen@gmail.com>
2021-10-27 20:59:56 +0200

    Added basic functionality to ToolGroup

--- src/main/java/model/tools/ToolGroup.java
+++ src/main/java/model/tools/ToolGroup.java
@@ -1,8 +1,48 @@
 package model.tools;
 
+import model.Lendable;
+
+import java.util.*;
+
 /**
  * Anouar Bannamar
  * 27-10-21
  */
-public class ToolGroup {
+public class ToolGroup extends Lendable {
+	
+	private String name;
+	private final Set<Lendable> tools;
+	
+	public ToolGroup (String name) {
+		setName (name);
+		tools = new HashSet<> ();
+	}
+	
+	public void setName (String name) {
+		if (name == null || !name.equals (""))
+			throw new IllegalArgumentException ("Name cannot be empty");
+		this.name = name;
+	}
+	
+	public boolean add (Lendable tool) {
+		return tools.add (tool);
+	}
+	
+	public boolean remove (Lendable tool) {
+		return tools.remove (tool);
+	}
+	
+	public int getSize () {
+		return tools.size ();
+	}
+	
+	public String getName () {
+		return name;
+	}
+	
+	public double getPrice () {
+		return tools.stream ().map(Lendable::getPrice).mapToDouble (value -> value).sum ();
+	}
+	
+	
 }
