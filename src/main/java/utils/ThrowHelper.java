978abb7a854842dfdb518a86c0e176ff44cbae7d
Jonas Leijzen <jonasleijzen@gmail.com>
2021-10-29 23:54:59 +0200

    More ThrowHelper functions

--- src/main/java/utils/ThrowHelper.java
+++ src/main/java/utils/ThrowHelper.java
@@ -1,5 +1,6 @@
 package utils;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 /**
@@ -8,24 +9,39 @@ import java.time.LocalDateTime;
  */
 public class ThrowHelper {
 	
-	public static void ThrowIfNullOrEmpty (String argName, String toCheck) {
-		if (toCheck == null || toCheck.equals (""))
+	public static void ThrowIfNull (String argName, Object object) {
+		if (object == null)
+			throw new NullPointerException (String.format ("%s cannot be null.", argName));
+	}
+	
+	public static void ThrowIfNullOrEmpty (String argName, String string) {
+		if (string == null || string.equals (""))
 			throw new IllegalArgumentException (String.format ("%s cannot be null or empty.", argName));
+	}
+	
+	public static void ThrowIfInThePast (String argName, LocalDate time) {
+		if (time.isBefore (LocalDate.now ()))
+			throw new IllegalArgumentException (String.format ("%s cannot be in the past.", time));
+	}
+	
+	public static void ThrowIfInTheFuture (String argName, LocalDate time) {
+		if (time.isAfter (LocalDate.now ()))
+			throw new IllegalArgumentException (String.format ("%s cannot be in the future.", time));
 	}
 	
 	public static void ThrowIfInThePast (String argName, LocalDateTime time) {
 		if (time.isBefore (LocalDateTime.now ()))
-			throw new IllegalArgumentException (String.format ("%s cannot be in the past", time));
+			throw new IllegalArgumentException (String.format ("%s cannot be in the past.", time));
 	}
 	
 	public static void ThrowIfInTheFuture (String argName, LocalDateTime time) {
 		if (time.isAfter (LocalDateTime.now ()))
-			throw new IllegalArgumentException (String.format ("%s cannot be in the future", time));
+			throw new IllegalArgumentException (String.format ("%s cannot be in the future.", time));
 	}
 	
 	public static void ThrowIfNegative (String argName, Number number) {
 		if (number.doubleValue () < 0)
-			throw new IllegalArgumentException (String.format ("%s cannot be negative", argName));
+			throw new IllegalArgumentException (String.format ("%s cannot be negative.", argName));
 	}
 	
 }
