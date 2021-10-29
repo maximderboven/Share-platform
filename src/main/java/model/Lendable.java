b1c4c170f475233f1a8b60a405b365a05d83e870
Jonas Leijzen <jonasleijzen@gmail.com>
2021-10-29 14:55:13 +0200

    Added attachments to lendable

--- src/main/java/model/Lendable.java
+++ src/main/java/model/Lendable.java
@@ -1,10 +1,26 @@
 package model;
 
+import java.util.*;
+
 /**
  * Arne Cools
  * 27/10/2021
  */
 public abstract class Lendable {
+	
+	private Set<Attachment> attachments = new HashSet<> ();
+	
+	public boolean addAttachment (Attachment attachment) {
+		return attachments.add (attachment);
+	}
+	
+	public boolean removeAttachment (Attachment attachment) {
+		return attachments.remove (attachment);
+	}
+	
+	public List<Attachment> getAllAttachments () {
+		return new ArrayList<> (attachments);
+	}
 	
 	public abstract double getPrice ();
 	
