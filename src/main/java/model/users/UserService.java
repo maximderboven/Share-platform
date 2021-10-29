d4ad3d8bc6f47ae5cb97a6726b3148fbe30a19b2
Jonas Leijzen <jonasleijzen@gmail.com>
2021-10-29 14:23:37 +0200

    Improved error handling in UserService

--- src/main/java/model/users/UserService.java
+++ src/main/java/model/users/UserService.java
@@ -7,9 +7,13 @@ package model.users;
 public class UserService {
 	
 	public void TransferSharepoints (User source, User receiver, double amount) {
-		if (source.getSharepoints () < amount)
+		if (amount < 0)
+			throw new IllegalArgumentException ("Amount cannot be negative");
+		try {
+			source.removeSharepoints (amount);
+		} catch (InsufficientFundsException e) {
 			throw new InsufficientFundsException ("The paying user not have enough sharepoints in their wallet.");
-		source.removeSharepoints (amount);
+		}
 		receiver.addSharepoints (amount);
 	}
 	
