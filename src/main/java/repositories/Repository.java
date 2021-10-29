96c93fc249e94dfe42dc01f519c6c204b1cf12d6
Jonas Leijzen <jonasleijzen@gmail.com>
2021-10-29 15:12:22 +0200

    Added repository interface

---
+++ src/main/java/repositories/Repository.java
@@ -0,0 +1,25 @@
+package repositories;
+
+import java.util.*;
+
+/**
+ * Jonas Leijzen
+ * 29/10/2021
+ */
+public abstract class Repository<T> {
+	
+	private Set<T> tSet = new HashSet<> ();
+	
+	public boolean add (T t) {
+		return tSet.add (t);
+	}
+	
+	public boolean remove (T t) {
+		return tSet.remove (t);
+	}
+	
+	public List<T> getAll () {
+		return new ArrayList<> (tSet);
+	}
+	
+}
