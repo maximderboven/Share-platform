3b0488841b502067a8c6f16777269038d17025ec
Jonas Leijzen <jonasleijzen@gmail.com>
2021-10-29 23:00:32 +0200

    Added generation of transaction lines via Transaction

--- src/main/java/model/transactions/Transaction.java
+++ src/main/java/model/transactions/Transaction.java
@@ -1,6 +1,7 @@
 package model.transactions;
 
 import model.tools.Lendable;
+import model.tools.Tool;
 import repositories.MultiClass;
 
 import java.time.LocalDateTime;
@@ -10,12 +11,30 @@ import java.time.LocalDateTime;
  * 27-10-21
  */
 public class Transaction {
-    private LocalDateTime time;
-    private Lendable lendable;
-    private double amount;
     private final MultiClass<TransactionLine> transactionLines = new MultiClass<> ();
+    
     
     public MultiClass<TransactionLine> getTransactionLines () {
         return transactionLines;
+    }
+    
+    public void generatePickupTransactionLinesForLendable (Lendable lendable) {
+        generateTransactionLines (TransactionType.PICKUP, lendable);
+    }
+    
+    public void generateReturnTransactionLinesForLendable (Lendable lendable) {
+        generateTransactionLines (TransactionType.RETURN, lendable);
+    }
+    
+    private void generateTransactionLines (TransactionType type, Lendable lendable) {
+        // Remove all Transaction lines of given TransactionType
+        for (TransactionLine line : transactionLines.getAll ())
+            if (line.getType () == type)
+                transactionLines.remove (line);
+        // (Re)add all transaction lines for lendable
+        for (Tool tool : lendable.getAllTools ()) {
+            TransactionLine line = new TransactionLine (LocalDateTime.now (), tool, type);
+            transactionLines.add (line);
+        }
     }
 }
