49f41f15ad25fedc0b12aeb658cfe44284bc70db
Jonas Leijzen <jonasleijzen@gmail.com>
2021-10-29 13:46:23 +0200

    Added basic functionality to Wallet

--- src/main/java/model/users/Wallet.java
+++ src/main/java/model/users/Wallet.java
@@ -6,6 +6,28 @@ package model.users;
  */
 public class Wallet {
 	
-
+	private double sharepoints;
+	
+	Wallet () {
+		sharepoints = 0;
+	}
+	
+	void addSharepoints (double amount) {
+		setSharepoints (sharepoints + amount);
+	}
+	
+	void removeSharepoints (double amount) {
+		setSharepoints (sharepoints - amount);
+	}
+	
+	double getSharepoints () {
+		return sharepoints;
+	}
+	
+	private void setSharepoints (double amount) {
+		if (amount < 0)
+			throw new IllegalArgumentException ("Sharepoints amount cannot be negative.");
+		sharepoints = amount;
+	}
 	
 }
