f4ef5a10625dda02af3fed8a596d4ebfd1f54b78
Jonas Leijzen <jonasleijzen@gmail.com>
2021-10-29 14:26:48 +0200

    Redid insufficient funds checking in userservice

--- src/main/java/model/users/UserService.java
+++ src/main/java/model/users/UserService.java
@@ -9,11 +9,9 @@ public class UserService {
 	public void TransferSharepoints (User source, User receiver, double amount) {
 		if (amount < 0)
 			throw new IllegalArgumentException ("Amount cannot be negative");
-		try {
-			source.removeSharepoints (amount);
-		} catch (InsufficientFundsException e) {
+		if (source.getSharepoints () < amount)
 			throw new InsufficientFundsException ("The paying user does not have enough sharepoints in their wallet.");
-		}
+		source.removeSharepoints (amount);
 		receiver.addSharepoints (amount);
 	}
 	
