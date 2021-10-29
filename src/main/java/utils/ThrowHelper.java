466fc50a738aff1a77e21848e4f265e4a7bc08bc
Jonas Leijzen <jonasleijzen@gmail.com>
2021-10-29 23:50:35 +0200

    Added more functions ThrowHelper

--- src/main/java/utils/ThrowHelper.java
+++ src/main/java/utils/ThrowHelper.java
@@ -1,5 +1,7 @@
 package utils;
 
+import java.time.LocalDateTime;
+
 /**
  * Jonas Leijzen
  * 29/10/2021
@@ -9,6 +11,21 @@ public class ThrowHelper {
 	public static void ThrowIfNullOrEmpty (String argName, String toCheck) {
 		if (toCheck == null || toCheck.equals (""))
 			throw new IllegalArgumentException (String.format ("%s cannot be null or empty.", argName));
+	}
+	
+	public static void ThrowIfInThePast (String argName, LocalDateTime time) {
+		if (time.isBefore (LocalDateTime.now ()))
+			throw new IllegalArgumentException (String.format ("%s cannot be in the past", time));
+	}
+	
+	public static void ThrowIfInTheFuture (String argName, LocalDateTime time) {
+		if (time.isAfter (LocalDateTime.now ()))
+			throw new IllegalArgumentException (String.format ("%s cannot be in the future", time));
+	}
+	
+	public static void ThrowIfNegative (String argName, Number number) {
+		if (number.doubleValue () < 0)
+			throw new IllegalArgumentException (String.format ("%s cannot be negative", argName));
 	}
 	
 }
