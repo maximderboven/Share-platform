7cffa4e4255355b907d6909e8007fa8109612fec
Jonas Leijzen <jonasleijzen@gmail.com>
2021-11-03 10:57:39 +0100

    Testdata naar feature verplaatst

--- src/test/java/be.kdg.cucumber/AfTeHalenGereedschapTonen.java
+++ src/test/java/be.kdg.cucumber/AfTeHalenGereedschapTonen.java
@@ -1,15 +1,20 @@
 package be.kdg.cucumber;
 
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import model.Reservation;
+import model.tools.Lendable;
 import model.tools.Tool;
 import model.tools.ToolType;
 import model.users.User;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Maxim Derboven
@@ -17,29 +22,9 @@ import java.time.format.DateTimeFormatter;
  */
 public class AfTeHalenGereedschapTonen {
     
-    User eveline, diederik;
-    
-    public AfTeHalenGereedschapTonen () {
-        configureTestData ();
-    }
-    
-    public void configureTestData () {
-        
-        eveline = new User ("Eveline", "", "");
-        eveline.addSharepoints (1000);
-        diederik = new User ("Diederik", "", "");
-        diederik.addSharepoints (1000);
-        
-        eveline.getLendables ().add (new Tool ("Kettingzaag", "", ToolType.ELECTRIC, 10));
-        eveline.getLendables ().add (new Tool ("Kettingzaag2", "", ToolType.ELECTRIC, 10));
-        eveline.getLendables ().add (new Tool ("Klopboormachine", "", ToolType.ELECTRIC, 20));
-        eveline.getLendables ().add (new Tool ("Keukenrobot", "", ToolType.KITCHEN, 15));
-        
-        diederik.getLendables ().add (new Tool ("PA Seismic", "", ToolType.SOUND_INSTALLATION, 200));
-        diederik.getLendables ().add (new Tool ("Black Strobo Magic", "", ToolType.LIGHT_INSTALLATION, 50));
-        
-        
-    }
+    Map<String, Lendable> lendableMap = new HashMap<> ();
+    Map<String, User> userMap = new HashMap<> ();
+    Map<String, Reservation> reservationMap = new HashMap<> ();
     
     LocalDate today;
     
@@ -52,20 +37,39 @@ public class AfTeHalenGereedschapTonen {
 
     @When("{string} aangeeft dat {string} zijn reservaties wil ophalen")
     public void aangeeftDatZijnReservatiesWilOphalen(String huurder, String verhuurder) {
-        this.huurder = new User(huurder);
-        this.verhuurder = new User(verhuurder);
-        verhuurder.getReservatie();
+    
     }
-
-    @Then("wordt een lijst met {int} reservaties getoond")
-    public void wordtEenLijstMetReservatiesGetoond(int arg0) {
+    
+    @Then ("wordt een lijst met {int} reservaties getoond")
+    public void wordtEenLijstMetReservatiesGetoond (int arg0) {
         // huh ?
     }
-
-    @And("bevat de lijst {string}")
-    public void bevatDeLijst(String arg0) {
+    
+    @And ("bevat de lijst {string}")
+    public void bevatDeLijst (String arg0) {
         //assert(verhuurder.getReservaties().stream().anyMatch(item -> "foo".equals(item.getName()));
     }
-
-
+    
+    
+    @Given ("Tools")
+    public void tools (DataTable table) {
+        for (Map<String, String> m : table.asMaps ()) {
+            Lendable l = new Tool (m.get ("name"), "", convertType (m.get ("type")), Double.parseDouble (m.get ("sp")));
+            lendableMap.put (m.get ("name"), l);
+        }
+    }
+    
+    private ToolType convertType (String str) {
+        switch (str) {
+            case "Elektrisch":
+                return ToolType.ELECTRIC;
+            case "Keuken":
+                return ToolType.KITCHEN;
+            case "Geluid":
+                return ToolType.SOUND_INSTALLATION;
+            case "Licht":
+                return ToolType.LIGHT_INSTALLATION;
+        }
+        throw new IllegalArgumentException ("Couldn't find tooltype.");
+    }
 }
--- src/test/resources/features/ToonAfTeHalenGereedschap.feature
+++ src/test/resources/features/ToonAfTeHalenGereedschap.feature
@@ -1,6 +1,16 @@
 Feature: Toon af te halen gereedschap
   Reservatie lijsten updaten
 
+  Background:
+    Given Tools
+      | name               | sp  | type       |
+      | Kettingzaag        | 10  | Elektrisch |
+      | Kettingzaag2       | 10  | Elektrisch |
+      | Klopboormachine    | 10  | Elektrisch |
+      | Keukenrobot        | 15  | Keuken     |
+      | PA Seismic         | 200 | Geluid     |
+      | Black Strobo Magic | 50  | Licht      |
+
   Scenario: Toon af te halen gereedschap
     Given today is "15/12/2021"
     When "Eveline" aangeeft dat "Diederik" zijn reservaties wil ophalen
