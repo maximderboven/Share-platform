593d83a7bd489cbc5a5c6f575f4064254fbd9708
Anouar Bannamar <anouar.bannamar@student.kdg.be>
2021-11-21 16:19:53 +0100

    typo

--- src/main/java/domein/transactie/HandelTransactieLijn.java
+++ src/main/java/domein/transactie/HandelTransactieLijn.java
@@ -5,6 +5,14 @@ package domein.transactie;
  * 21/11/2021
  */
 public class HandelTransactieLijn implements TransactieLijn {
+    private float bedrag;
+    private Handels
+
+
+    @Override
+    public long sharepoints() {
+        return 0;
+    }
 
 
 }
