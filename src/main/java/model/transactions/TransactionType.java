8372008b125138cf37703fc476262f7b3466b5dd
Jonas Leijzen <jonasleijzen@gmail.com>
2021-11-21 15:29:34 +0100

    Fresh start

--- src/main/java/model/transactions/TransactionType.java
+++
@@ -1,9 +0,0 @@
-package model.transactions;
-
-/**
- * Jonas Leijzen
- * 29/10/2021
- */
-public enum TransactionType {
-	PICKUP, RETURN
-}
--- src/main/java/model/users/Expert.java
+++
@@ -1,8 +0,0 @@
-package model.users;
-
-/**
- * Arne Cools
- * 27/10/2021
- */
-public class Expert extends Visitor{
-}
