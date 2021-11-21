871d44681a0d2f7bf4bb9c23bbf3d7a7244775a6
Anouar Bannamar <anouar.bannamar@student.kdg.be>
2021-11-21 15:53:29 +0100

    Gebruiker

---
+++ src/main/java/domein/gebruiker/Gebruiker.java
@@ -0,0 +1,18 @@
+package domein.gebruiker;
+
+import java.io.LineNumberInputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Anouar Bannamar
+ * 21-11-21
+ */
+public class Gebruiker {
+    private String login;
+    private GeoLocatie locatie;
+    private long sharepoints;
+
+    private Reservatie ontlener;
+
+}
