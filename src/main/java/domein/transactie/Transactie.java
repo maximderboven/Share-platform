e6b882907b6dd073592209e8932f902f7f6dbf0c
Maxim Derboven <maxim.derboven@student.kdg.be>
2021-11-24 09:59:49 +0100

    added method for adding Transactionlines to transaction

--- src/main/java/domein/transactie/Transactie.java
+++ src/main/java/domein/transactie/Transactie.java
@@ -11,32 +11,38 @@ import java.util.Set;
  * 21/11/2021
  */
 public class Transactie {
-	
-	private final int id;
-	private Gebruiker ontvanger;
-	private Gebruiker betaler;
-	private Reservatie reservatie;
-	private LocalDateTime tijdstip;
-	private final Set<TransactieLijn> lijnen;
-	
-	public Transactie (int id, Gebruiker ontvanger, Gebruiker betaler, Reservatie reservatie, LocalDateTime tijdstip) {
-		this.id = id;
-		this.ontvanger = ontvanger;
-		this.betaler = betaler;
-		this.reservatie = reservatie;
-		this.tijdstip = tijdstip;
-		lijnen = new HashSet<> ();
-	}
-	
-	public int getId () {
-		return id;
-	}
-	
-	public LocalDateTime getTijdstip () {
-		return tijdstip;
-	}
-	
-	public Set<TransactieLijn> getLijnen () {
-		return lijnen;
-	}
+
+    private final int id;
+    private Gebruiker ontvanger;
+    private Gebruiker betaler;
+    private Reservatie reservatie;
+    private LocalDateTime tijdstip;
+    private final Set<TransactieLijn> lijnen;
+
+    public Transactie(int id, Gebruiker ontvanger, Gebruiker betaler, Reservatie reservatie, LocalDateTime tijdstip) {
+        this.id = id;
+        this.ontvanger = ontvanger;
+        this.betaler = betaler;
+        this.reservatie = reservatie;
+        this.tijdstip = tijdstip;
+        lijnen = new HashSet<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public LocalDateTime getTijdstip() {
+        return tijdstip;
+    }
+
+    public Set<TransactieLijn> getLijnen() {
+        return lijnen;
+    }
+
+    private void AddTransactionLines(float bedrag, HandelsTypeLijn handelsTypeLijn) {
+        lijnen.add(new HandelTransactieLijn(bedrag, handelsTypeLijn));
+    }
+
+
 }
