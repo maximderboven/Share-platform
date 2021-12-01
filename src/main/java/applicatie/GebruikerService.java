c01d37baa9eccccaec61df3349348ad968f9f745
Jonas Leijzen <jonasleijzen@gmail.com>
2021-12-01 10:12:29 +0100

    schrijfSharepointsOver toegevoegd aan GebruikerService

--- src/main/java/applicatie/GebruikerService.java
+++ src/main/java/applicatie/GebruikerService.java
@@ -1,13 +1,23 @@
 package applicatie;
 
+import domein.gebruiker.Gebruiker;
+import domein.gebruiker.GebruikerCataloog;
+
 /**
  * Jonas Leijzen
  * 1/12/2021
  */
 public class GebruikerService {
 	
-	public static void transferSharepoints (String betalerLogin, String ontvangerLogin) {
+	private static GebruikerCataloog cataloog = new GebruikerCataloog ();
 	
+	public static void schrijfSharepointsOver (String betalerLogin, String ontvangerLogin, long amount) {
+		Gebruiker betaler = cataloog.getByLogin (betalerLogin);
+		Gebruiker ontvanger = cataloog.getByLogin (ontvangerLogin);
+		if (amount < 0)
+			throw new IllegalArgumentException ("Amount cannot be negative.");
+		betaler.setSharepoints (betaler.getSharepoints () - amount);
+		ontvanger.setSharepoints (betaler.getSharepoints () + amount);
 	}
 	
 }
