c568ed66eafb5757423589ab5dd58b1e36b64a8c
Jonas Leijzen <jonasleijzen@gmail.com>
2021-12-02 15:31:28 +0100

    Typo en swap gefixed

--- src/main/java/util/GeoLocatie.java
+++ src/main/java/util/GeoLocatie.java
@@ -6,31 +6,31 @@ package util;
  */
 public class GeoLocatie {
     private double longitude;
-    private double lattitude;
+    private double latitude;
     
     public GeoLocatie (double longitude, double lattitude) {
         setLongitude (longitude);
-        setLattitude (lattitude);
+        setLatitude (lattitude);
     }
     
     public double getLongitude () {
         return longitude;
     }
-
-    public void setLongitude(double longitude) {
-        if (longitude < -180 || longitude > 180)
+    
+    public void setLongitude (double longitude) {
+        if (longitude < -90 || longitude > 90)
             throw new IllegalArgumentException ("longitude cannot go beyond (-180, 180).");
         this.longitude = longitude;
     }
-
-    public double getLattitude() {
-        return lattitude;
+    
+    public double getLatitude () {
+        return latitude;
     }
-
-    public void setLattitude(double lattitude) {
-        if (lattitude < -90 || lattitude > 90)
+    
+    public void setLatitude (double latitude) {
+        if (latitude < -180 || latitude > 180)
             throw new IllegalArgumentException ("lattitude cannot go beyond (-90, 90).");
-        this.lattitude = lattitude;
+        this.latitude = latitude;
     }
 }
 
