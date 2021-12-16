5efc99914dde4d46282b5dfa109704b007c5cbc1
Jonas Leijzen <jonasleijzen@gmail.com>
2021-12-16 15:08:24 +0100

    Typo

--- src/main/java/persistence/GebruikerCataloog.java
+++ src/main/java/persistence/GebruikerCataloog.java
@@ -6,7 +6,7 @@ import domein.gebruiker.Gebruiker;
  * Jonas Leijzen
  * 16/12/2021
  */
-public interface GebruikersCataloog {
+public interface GebruikerCataloog {
 	
 	Gebruiker getByLogin (String login);
 	
