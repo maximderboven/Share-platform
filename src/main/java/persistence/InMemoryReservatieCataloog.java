094f893830859474c77f4e976a3695e171397dd7
Jonas Leijzen <jonasleijzen@gmail.com>
2021-12-16 16:10:35 +0100

    typo

--- src/main/java/persistence/InMemoryReservatieCataloog.java
+++ src/main/java/persistence/InMemoryReservatieCataloog.java
@@ -1,6 +1,5 @@
 package persistence;
 
-import domein.gebruiker.Gebruiker;
 import domein.transactie.Reservatie;
 
 import java.time.LocalDate;
@@ -8,52 +7,52 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 /**
  * @author Maxim Derboven
  * @version 1.0 16/12/2021 15:04
  */
 public class InMemoryReservatieCataloog implements ReservatieCataloog {
-
-	public Set<Reservatie> reservatieSet = new HashSet<>();
-
+	
+	public Set<Reservatie> reservatieSet = new HashSet<> ();
+	
 	@Override
-	public int Add(Reservatie reservatie) {
-		reservatieSet.add(reservatie);
+	public int add (Reservatie reservatie) {
+		reservatieSet.add (reservatie);
 		return reservatie.id;
 	}
-
+	
 	@Override
-	public boolean remove(int id) {
-		return reservatieSet.removeIf(t -> t.id == id);
+	public boolean remove (int id) {
+		return reservatieSet.removeIf (t -> t.id == id);
 	}
-
+	
 	@Override
-	public List<Reservatie> getAll() {
-		return new ArrayList<>(reservatieSet);
+	public List<Reservatie> getAll () {
+		return new ArrayList<> (reservatieSet);
 	}
-
+	
 	@Override
-	public Reservatie[] geefAfhaalbareReservaties(String aanbiederLogin, String ontlenerLogin, LocalDate datum) {
-		return getAll().stream().filter(
+	public Reservatie[] geefAfhaalbareReservaties (String aanbiederLogin, String ontlenerLogin, LocalDate datum) {
+		return getAll ().stream ().filter (
 						//r -> aanbiederLogin.equals (r.getGereedschap ().getAanbieder ().getLogin ())
-						r -> aanbiederLogin.equals(r.getAanbieder().getLogin())
-								&& ontlenerLogin.equals(r.getOntlener().getLogin())
-								&& r.isAfhaalbaar(datum))
-				.toArray(Reservatie[]::new);
+						r -> aanbiederLogin.equals (r.getAanbieder ().getLogin ())
+								&& ontlenerLogin.equals (r.getOntlener ().getLogin ())
+								&& r.isAfhaalbaar (datum))
+				.toArray (Reservatie[]::new);
 	}
-
+	
 	@Override
-	public Reservatie geefReservatie(int reservatieId) {
+	public Reservatie geefReservatie (int reservatieId) {
 		for (Reservatie r : reservatieSet) {
 			if (r.getId () == reservatieId)
 				return r;
 		}
 		return null;
 	}
-
-	private static Random randomIDs = new Random();
+	
+	private static Random randomIDs = new Random ();
+	
 	@Override
 	public int getNewId () {
 		int id;
